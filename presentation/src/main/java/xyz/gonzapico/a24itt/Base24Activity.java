fde6c20cb1ddac9f81d4328fa4c6916c88f0c8c6
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-25 17:23:51 +0100

    Added butterknife to base activity

--- presentation/src/main/java/xyz/gonzapico/a24itt/Base24Activity.java
+++ presentation/src/main/java/xyz/gonzapico/a24itt/Base24Activity.java
@@ -3,6 +3,7 @@ package xyz.gonzapico.a24itt;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
 
 /**
  * Created by gfernandez on 25/02/17.
@@ -13,6 +14,7 @@ public abstract class Base24Activity extends AppCompatActivity {
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(getLayoutResource());
+    ButterKnife.bind(this);
   }
 
   protected abstract int getLayoutResource();

3c3eea244302181445c8178e4b648b70cdd7439d
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-27 09:07:34 +0100

    Implementation of repository on data layer

--- data/src/main/java/xyz/gonzapico/data/entity/mapper/MovieMapper.java
+++ data/src/main/java/xyz/gonzapico/data/entity/mapper/MovieMapper.java
@@ -1,8 +1,18 @@
 package xyz.gonzapico.data.entity.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Response;
+import xyz.gonzapico.data.entity.MovieAPIResponse;
+import xyz.gonzapico.entity.MovieDomainEntity;
+
 /**
  * Created by gfernandez on 26/02/17.
  */
 
 public class MovieMapper {
+
+  public List<MovieDomainEntity> transformToListOfMovies(Response<MovieAPIResponse> responseOfAPI) {
+    return new ArrayList<>();
+  }
 }

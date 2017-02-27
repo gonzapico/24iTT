6b170602efbfe6a54362a50c87d0265dea203258
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-27 09:07:11 +0100

    Refactor done

--- domain/src/main/java/xyz/gonzapico/interactor/GetPopularMovies.java
+++ domain/src/main/java/xyz/gonzapico/interactor/GetPopularMovies.java
@@ -4,7 +4,7 @@ import javax.inject.Inject;
 import rx.Observable;
 import xyz.gonzapico.executor.PostExecutionThread;
 import xyz.gonzapico.executor.ThreadExecutor;
-import xyz.gonzapico.repository.MoviesRepository;
+import xyz.gonzapico.repository.MoviesDomainRepository;
 
 /**
  * Created by gfernandez on 27/02/17.
@@ -12,12 +12,12 @@ import xyz.gonzapico.repository.MoviesRepository;
 
 public class GetPopularMovies extends BaseUseCase {
 
-  private final MoviesRepository mRepository;
+  private final MoviesDomainRepository mRepository;
 
-  @Inject public GetPopularMovies(MoviesRepository moviesRepository, ThreadExecutor threadExecutor,
+  @Inject public GetPopularMovies(MoviesDomainRepository moviesDomainRepository, ThreadExecutor threadExecutor,
       PostExecutionThread postExecutionThread) {
     super(threadExecutor, postExecutionThread);
-    this.mRepository = moviesRepository;
+    this.mRepository = moviesDomainRepository;
   }
 
   @Override public Observable buildUseCaseObservable() {

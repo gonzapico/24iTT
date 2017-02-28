44bcef7de65bef081437f8dda471f16cca055193
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-28 08:52:59 +0100

    Injecting navigator

--- presentation/src/main/java/xyz/gonzapico/a24itt/Base24Activity.java
+++ presentation/src/main/java/xyz/gonzapico/a24itt/Base24Activity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import butterknife.ButterKnife;
+import javax.inject.Inject;
 import xyz.gonzapico.a24itt.di.components.ApplicationComponent;
 import xyz.gonzapico.a24itt.di.modules.ActivityModule;
+import xyz.gonzapico.a24itt.navigation.Navigator;
 
 /**
  * Created by gfernandez on 25/02/17.
  */
 
 public abstract class Base24Activity extends AppCompatActivity {
+
+  @Inject Navigator mNavigator;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);

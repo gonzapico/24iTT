01ffacd6e61a60b82187a43f97fb9f5dbb353cd2
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-28 08:54:20 +0100

    Model is parcelable from now on

--- presentation/src/main/java/xyz/gonzapico/a24itt/listPopularMovies/MovieModel.java
+++ presentation/src/main/java/xyz/gonzapico/a24itt/listPopularMovies/MovieModel.java
@@ -1,10 +1,13 @@
 package xyz.gonzapico.a24itt.listPopularMovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by gfernandez on 25/02/17.
  */
 
-public class MovieModel {
+public class MovieModel implements Parcelable {
 
   private int id;
   private String poster = "";
@@ -39,4 +42,31 @@ public class MovieModel {
   public void setOverview(String overview) {
     this.overview = overview;
   }
+
+  @Override public int describeContents() {
+    return 0;
+  }
+
+  @Override public void writeToParcel(Parcel dest, int flags) {
+    dest.writeInt(this.id);
+    dest.writeString(this.poster);
+    dest.writeString(this.overview);
+  }
+
+  protected MovieModel(Parcel in) {
+    this.id = in.readInt();
+    this.poster = in.readString();
+    this.overview = in.readString();
+  }
+
+  public static final Parcelable.Creator<MovieModel> CREATOR =
+      new Parcelable.Creator<MovieModel>() {
+        @Override public MovieModel createFromParcel(Parcel source) {
+          return new MovieModel(source);
+        }
+
+        @Override public MovieModel[] newArray(int size) {
+          return new MovieModel[size];
+        }
+      };
 }

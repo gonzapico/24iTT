1b998bccca772c86c08bc975308ef2e7739e3814
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-02-28 17:02:37 +0100

    MVP for this feature

---
+++ presentation/src/main/java/xyz/gonzapico/a24itt/watchTrailer/TrailerModel.java
@@ -0,0 +1,18 @@
+package xyz.gonzapico.a24itt.watchTrailer;
+
+/**
+ * Created by gfernandez on 28/02/17.
+ */
+
+public class TrailerModel {
+
+  String key = "";
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+}
---
+++ presentation/src/main/java/xyz/gonzapico/a24itt/watchTrailer/TrailerView.java
@@ -0,0 +1,10 @@
+package xyz.gonzapico.a24itt.watchTrailer;
+
+/**
+ * Created by gfernandez on 28/02/17.
+ */
+
+public interface TrailerView {
+
+  void showVideo(String idVideo);
+}
